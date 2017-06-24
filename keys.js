ad3cf3b5e4d41155d939ff65b6425d8ddf089b09
michelleryan <mryan85268@gmail.com>
2017-06-23 23:08:59 -0700

    all functions working

--- keys.js
+++ keys.js
@@ -6,3 +6,12 @@ exports.twitterKeys = {
   access_token_key: '876976172541726720-t0t0kqp6an9TsNO2C9MLW0CsjrCXjFA',
   access_token_secret: 'f9gym2viLwxJMBFDrfJKivBZalGG6o3s8vOWqLL8SJKLY',
 }
+
+exports.spotifyKeys = {
+	id: '358928bfe2ff4937878928799ebd0cf9',
+	secret: '1fcd8a5b0ee14b12b25c68aa0f08bafe'
+}
+
+exports.movieKeys = {
+	apikey:'40e9cece'
+}
--- liri.js
+++ liri.js
@@ -27,6 +27,16 @@ var client = new twitter({
   access_token_secret: twKeys.access_token_secret
 });
 
+var spotKeys = keys.spotifyKeys;
+var spotifyClient = new spotify({
+	id: spotKeys.id,
+	secret: spotKeys.secret
+});
+//console.log(spotKeys.id);
+
+var movieKeys = keys.movieKeys;
+//console.log(movieKeys.apikey);
+
 //post tweets into my twitter account
 // var tweetInput = process.argv[3];
 // var tparam = {status: tweetInput};
@@ -38,7 +48,9 @@ var client = new twitter({
 // 	  //console.log(response);  // Raw response object. 
 // 	});
 // }
-//get my tweets
+
+
+//get my last 20 tweets, show tweet and created date
 if (command == 'my-tweets') {
 	var params = {screen_name: 'mryan85268'};
 	client.get('statuses/user_timeline', params, function(error, tweets, response){
@@ -46,11 +58,98 @@ if (command == 'my-tweets') {
 			//print out the last 20 tweets
 			//console.log("tweet: " + tweets[0].text);
 			for (let i=0; i<20; i++) {
-				console.log("tweet: " + tweets[i].text + ",Created at: " + tweets[i].created_at);
+				var text = "tweet: " + tweets[i].text + ",Created at: " + tweets[i].created_at;
+				console.log(text);
+				writeLog(command, text);
+
 			}
 			
 		}
 	});
+
 }
 
+//spotify song
+if (command == 'spotify-this-song') {
+	var songInput = process.argv[3];
 
+	if (songInput==null) {
+		console.log("Use default song");
+		var song = {type: 'track', query:'The Sign'};
+	}
+	else{
+		var song = {
+		type:'track',
+		query: songInput
+		}
+	}
+	
+
+	spotifyClient.search(song, function(error, data){
+		if (!error) {
+			var songInfo = data.tracks.items[0];
+			console.log(songInfo.artists[0].name);
+			console.log(songInfo.name);
+			console.log(songInfo.href);
+			console.log(songInfo.album.name);
+		}
+	});
+
+}
+
+//get a movie from ODMB
+if (command=='movie-this') {
+	var movieInput = process.argv[3];
+
+	if (movieInput==null) {
+		console.log("Use default movie");
+		request('http://www.omdbapi.com/?apikey=40e9cece&t="Mr. Nobody"', function (error, data, body) {
+  			if (!error) {
+  				console.log(body);  //I could not get this to print the specific items requested!!
+  			}
+		});
+
+	}
+	else {
+		request('http://www.omdbapi.com/?apikey=40e9cece&t=' + movieInput, function(error, data, body){
+			if (!error) {
+				console.log(body); //I could not get this to print the specific items requested!!
+			}
+		})
+	}
+
+}
+
+//do what it says 
+if (command=='do-what-it-says') {
+	fs.readFile("random.txt", "utf8", function (err, data) { 
+
+		if(err) {
+			return console.log(err);
+		}
+		
+		var randomArr = data.split(",");
+		var song = {
+			type:'track',
+			query: randomArr[1]
+			}
+		spotifyClient.search(song, function(error, data){
+			if (!error) {
+				var songInfo = data.tracks.items[0];
+				console.log(songInfo.artists[0].name);
+				console.log(songInfo.name);
+				console.log(songInfo.href);
+				console.log(songInfo.album.name);
+				}
+			});
+			
+	});
+}
+
+function writeLog (command, text) {
+	fs.appendFile('log.txt', command + ', ' + text + "\n", function(err){
+					if (err) {
+						console.log(err);
+					}
+				});
+}
